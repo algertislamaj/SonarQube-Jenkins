49eb82c85d11d875382021d149514110b304014f
Algert <algertislamaj@gmail.com>
2023-07-10 19:06:57 +0200

    second commit

---
+++ BE/src/main/java/com/pero/Wien/WienApplication.java
@@ -0,0 +1,13 @@
+package com.pero.Wien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WienApplication.class, args);
+	}
+
+}
---
+++ BE/src/test/java/com/pero/Wien/WienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pero.Wien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
